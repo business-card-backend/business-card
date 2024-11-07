d53d6494deede3802c236a0a13b92d035494594c
Kyumin Kim <lemon6565@naver.com>
2024-11-07 21:31:46 +0900

    fix: remove EMAIL, LOGIN_TYPE (Patch API Request/Response)

--- src/main/java/solverz/business_card/domain/member/request/PatchMemberRequest.java
+++ src/main/java/solverz/business_card/domain/member/request/PatchMemberRequest.java
@@ -7,9 +7,6 @@ import solverz.business_card.domain.member.entity.LoginType;
 
 @Getter
 public class PatchMemberRequest {
-    @Schema(description = "사용자 등록 이메일", defaultValue = "test2@mail.com")
-    private String email;
-
     @Schema(description = "사용자 계정 패스워드", defaultValue = "1234")
     private String password;
 
@@ -18,8 +15,5 @@ public class PatchMemberRequest {
 
     @Schema(description = "사용자 명함 이미지 URL", defaultValue = "hi.png")
     private String nameCardImgUrl;
-
-    @Schema(description = "사용자 로그인 타입 (EMAIL, GMAIL, APPLE_MAIL)", defaultValue = "EMAIL")
-    private LoginType loginType;
 }
 
--- src/main/java/solverz/business_card/domain/member/response/PatchMemberResponse.java
+++ src/main/java/solverz/business_card/domain/member/response/PatchMemberResponse.java
@@ -9,9 +9,6 @@ import solverz.business_card.domain.member.entity.Member;
 @Getter
 @Builder
 public class PatchMemberResponse {
-    @Schema(description = "사용자 등록 이메일")
-    private String email;
-
     @Schema(description = "사용자 계정 패스워드")
     private String password;
 
@@ -21,16 +18,11 @@ public class PatchMemberResponse {
     @Schema(description = "사용자 명함 이미지 URL")
     private String nameCardImgUrl;
 
-    @Schema(description = "사용자 로그인 타입 (EMAIL, GMAIL, APPLE_MAIL)")
-    private LoginType loginType;
-
     public static PatchMemberResponse memberToResponse(Member member) {
         return PatchMemberResponse.builder()
-                .email(member.getEmail())
                 .password(member.getPassword())
                 .name(member.getName())
                 .nameCardImgUrl(member.getNameCardImgUrl())
-                .loginType(member.getLoginType())
                 .build();
     }
 }

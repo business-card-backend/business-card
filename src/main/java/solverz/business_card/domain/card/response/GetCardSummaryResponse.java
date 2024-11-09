dc7530fe21347da305331288d9fc7c044d851649
csb0710 <sdssond@gmail.com>
2024-11-10 02:56:14 +0900

    feat: 멤버 소유 명함 목록 요청 API

---
+++ src/main/java/solverz/business_card/domain/card/response/GetCardSummaryResponse.java
@@ -0,0 +1,30 @@
+package solverz.business_card.domain.card.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import solverz.business_card.domain.card.entity.Card;
+
+@Builder
+@Schema(description = "명함 요약 정보 응답")
+public record GetCardSummaryResponse(
+        @Schema(description = "명함 이미지 주소")
+        String cardImgURL,
+
+        @Schema(description = "명함 회사 이름")
+        String companyName,
+
+        @Schema(description = "명함 회사 주소")
+        String companyAddress,
+
+        @Schema(description = "명함 회사 번호")
+        String phoneNumber
+) {
+        public static GetCardSummaryResponse from(Card card) {
+            return GetCardSummaryResponse.builder()
+                    .cardImgURL(card.getCardImgURL())
+                    .companyName(card.getCompanyName())
+                    .companyAddress(card.getCompanyAddress())
+                    .phoneNumber(card.getPhoneNumber())
+                    .build();
+        }
+}

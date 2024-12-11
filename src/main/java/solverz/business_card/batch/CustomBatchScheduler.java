cb7fae9e7bc82adc338587cb9668da45a0bec627
Kyumin Kim <lemon6565@naver.com>
2024-12-11 16:35:55 +0900

    fix: batch parameter 추가 (재시작문제)

--- src/main/java/solverz/business_card/batch/CustomBatchScheduler.java
+++ src/main/java/solverz/business_card/batch/CustomBatchScheduler.java
@@ -1,9 +1,12 @@
 package solverz.business_card.batch;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.Job;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class CustomBatchScheduler {
@@ -18,6 +21,10 @@ public class CustomBatchScheduler {
     @Scheduled(cron = "0 * * * * *") // 1분마다 실행 (테스트)
     //@Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
     public void runDeleteExpiredMembersJob() throws Exception {
-        jobLauncher.run(deleteExpiredMembersJob, new JobParameters());
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("time", LocalDateTime.now().toString()) // 고유한 파라미터 추가
+                .toJobParameters();
+
+        jobLauncher.run(deleteExpiredMembersJob, jobParameters);
     }
 }

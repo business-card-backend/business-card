205ff65bb0869670fa7afeb4a7547d82bff84456
Kyumin Kim <lemon6565@naver.com>
2025-01-02 01:17:38 +0900

    feat: 로깅, 웹훅을 위한 파일 추가

---
+++ src/main/java/solverz/business_card/domain/common/logging/HTTPFilter.java
@@ -0,0 +1,26 @@
+package solverz.business_card.domain.common.logging;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+@Component
+public class HTTPFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain chain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        chain.doFilter(requestWrapper, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
+    }
+}

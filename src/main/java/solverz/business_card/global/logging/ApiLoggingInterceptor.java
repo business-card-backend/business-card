b80fa4750f9d0b15ae5f1c21d5c1a311b7118804
Kyumin Kim <lemon6565@naver.com>
2025-01-04 23:02:37 +0900

    fix: 기존 api intercept 코드에 디스코드 웹훅 추가

--- src/main/java/solverz/business_card/global/logging/ApiLoggingInterceptor.java
+++ src/main/java/solverz/business_card/global/logging/ApiLoggingInterceptor.java
@@ -2,20 +2,27 @@ package solverz.business_card.global.logging;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@RequiredArgsConstructor
 @Component
 public class ApiLoggingInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(ApiLoggingInterceptor.class);
+    private final DiscordMessageProvider discordMessageProvider;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String method = request.getMethod();
         String uri = request.getRequestURI();
-        logger.info("API Called: [{} {}]", method, uri);
+        String logMessage = String.format("API Called: [%s %s]", method, uri);
+        logger.info(logMessage);
+
+        // discord trigger
+        discordMessageProvider.sendMessage(logMessage);
         return true;
     }
 }
